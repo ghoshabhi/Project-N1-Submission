0d1255b6dee5b96a3d94aebfe4a82cd8df68b7a7
Abhishek Ghosh <abhighosh18@gmail.com>
2017-04-25 15:49:21 -0700

    Setup server

---
+++ index.js
@@ -0,0 +1,30 @@
+const pg = require('pg');
+const express = require('express');
+
+const app = express();
+
+app.use('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.redirect('/db');
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM students', function (err, result) {
+      done();
+      if (err) {
+        console.error("An error occurred on the server. Error: " + err);
+      } else {
+        const students = result.rows;
+        response.render('pages/index', { students: students });
+      }
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT} 😎 ...`);
+});
